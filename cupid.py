40944a85600b290846405c718c1154641c6df77b
Tevin Mukudi <64235935+Ven011@users.noreply.github.com>
2025-02-26 12:43:14 -0500

    convert date profile weight to ounces

--- cupid.py
+++ cupid.py
@@ -270,7 +270,8 @@ class Cupid:
             self.base_profiles, _ = self.detect_dates()
 
         for profile in self.base_profiles:
-            cv2.putText(self.compare_frame, str(round(profile.est_weight, 1)), tuple(profile.position), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            # put weight text over dates profiles in ounces
+            cv2.putText(self.compare_frame, str(round((profile.est_weight * 0.03527396), 2)), tuple(profile.position), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
         count_line = int((end_x - start_x) - settings.return_counter_setting("count_line_offset"))
         cv2.line(self.compare_frame, (0, count_line), (end_y, count_line), (255, 255, 255), thickness=1)
